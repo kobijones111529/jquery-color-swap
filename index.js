3ee2ac3b965aa827126b504cec6970d2468dd782
Kobi Jones <kobijonesmail@gmail.com>
2023-04-03 13:34:42 -0500

    Add counter to generate button

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-  $('#generate-btn').on('click', function() {
-    
+  $('#generate-btn').data('count', 0);
+  $('#generate-btn').on('click', function(e) {
+    // Update count
+    const jqElem = $(e.target);
+    const count = jqElem.data('count');
+    jqElem.data('count', count + 1)
+
+    $(document.body).append(`
+      <div>
+        <p>${jqElem.data('count')}</p>
+      </div>
+    `);
   });
 });
